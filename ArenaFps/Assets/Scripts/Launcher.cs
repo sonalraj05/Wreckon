d33d706c268fa2800bbc7ec35f8f0469538054a7
sonalrajapaksha <62091838+sonalraj05@users.noreply.github.com>
2023-05-14 23:10:38 +1200

    Did some work on the room creation aspect of the menu
    had to look at some photon documentation to figure out how the room creation worked

--- ArenaFps/Assets/Scripts/Launcher.cs
+++ ArenaFps/Assets/Scripts/Launcher.cs
@@ -2,10 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using TMPro;
 
 public class Launcher : MonoBehaviourPunCallbacks
 {
-    // Start is called before the first frame update
+    [SerializeField] TMP_InputField roomNameInputField;
+    [SerializeField] TMP_Text errorText;
+    [SerializeField] TMP_Text roomNameText;
+
     void Start()
     {
         Debug.Log("Connecting To Master");
@@ -26,8 +30,37 @@ public class Launcher : MonoBehaviourPunCallbacks
     }
 
     // Update is called once per frame
-    void Update()
+    public void CreateRoom()
     {
-        
+        if(string.IsNullOrEmpty(roomNameInputField.text))
+        {
+            return;
+        }
+        PhotonNetwork.CreateRoom(roomNameInputField.text);
+        MenuManager.Instance.OpenMenu("loading");
     }
+
+    public override void OnJoinedRoom()
+    {
+        MenuManager.Instance.OpenMenu("room");
+        roomNameText.text = PhotonNetwork.CurrentRoom.Name;
+
+    }
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        errorText.text = "Room Creation Failed: " + message;
+        MenuManager.Instance.OpenMenu("error");
+    }
+
+    public void LeaveRoom()
+    {
+        PhotonNetwork.LeaveRoom();
+        MenuManager.Instance.OpenMenu("loading");
+    }
+
+    public override void OnLeftRoom()
+    {
+        MenuManager.Instance.OpenMenu("title");
+    }
+
 }

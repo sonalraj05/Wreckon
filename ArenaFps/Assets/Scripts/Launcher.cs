09b063ea6318ccda3cdd4b16873b7139a944cf7b
sonalrajapaksha <62091838+sonalraj05@users.noreply.github.com>
2023-05-14 16:54:15 +1200

    Created Menu
    The Photon Pun System is working and connects to the AUS servers
    
    Also the main menu is basically finished with all the buttons and I just need to link them to other scenes to finish it.
    
    Last Year we did a moving background so we are thinking of implementing is again this year. This will happen later when the map is finished.

---
+++ ArenaFps/Assets/Scripts/Launcher.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class Launcher : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Connecting To Master");
+        PhotonNetwork.ConnectUsingSettings();
+    }
+    
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected To Master");
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        MenuManager.Instance.OpenMenu("title");
+        Debug.Log("Joined Lobby");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
